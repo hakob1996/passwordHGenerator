b649689f690647194d68277ad42c14983f18bb5e
Hakob Muradyan <hakobmuradyan1996@gmail.com>
2019-10-03 01:29:54 -0700

    i didnt really understand homework instruction , now i have fixed it :)

--- script.js
+++ script.js
@@ -1,18 +1,100 @@
 function password1(){
-    
-    var character = "qwertyuiopafghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890[]|.$#@!%^&*()_-=+/?<>,{}";
+    var lowerCase = "qwertyuiopasdfghjklzxcvbnm";
+    var upperCase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+    var numbers = "1234567890";
+    var elses = "[]|.$#@!%^&*()_-=+/?<>,{}";
+    var allTogether = "qwer[]|.$#@!%^&*(tyuiopafgFGHJKLZXhjklzxcvbASDCVBNM1234567890)_-=+/?<>,{}";
     var generated = "";
+    var question = prompt("How many characters would you like?");
+    var askLowerCase = confirm("Press OK if You would like just lowercase");
+    
 
-    for(var i = 0, n = character.length; i < 8; i++){
-        generated += character.charAt(Math.floor(Math.random() * n));  
+
+
+   
+ 
+function together(){
+        for(var i = 0, n = question; i < question; i++){
+            generated += allTogether.charAt(Math.floor(Math.random() * n));  
+            document.getElementById("password").innerHTML = generated;
+            
+            
+        }
+        
     }
-   document.getElementById("password").innerHTML = generated;
-}
 
-function copy1(){
-   password.style.fontSize="45px";
-   var copy = document.getElementById("password");
-   copy.select();
-   document.execCommand("copy");
+function upperCase1(){
+        var askUpperCase = confirm("Press OK if You would like just uppercase");
+    
+        if (askUpperCase){
+            for(var i = 0, n = question; i < question; i++){
+                generated += upperCase.charAt(Math.floor(Math.random() * n)); 
+                document.getElementById("password").innerHTML = generated; 
+                
+                }
+                
+            }
+            else  {
+                
+                number();
+            }
+     }
+    
+function chracter(){
+        var askCharacters = confirm("Press OK if You would like just chracters");
+        if(askCharacters){
+            for(var i = 0, n = question; i < question; i++){
+                generated += elses.charAt(Math.floor(Math.random() * n));  
+                document.getElementById("password").innerHTML = generated;
+                
+                
+            }
+            
+        }
+        else{
+            together();
+        }
+    }
+function number(){
+        var askNumbers = confirm("Press OK if You would like just numbers");
+        if(askNumbers){
+            for(var i = 0, n = question; i < question; i++){
+                generated += numbers.charAt(Math.floor(Math.random() * n));  
+                document.getElementById("password").innerHTML = generated;
+                
+                
+            }
+            
+        }
+        else{
+            
+            chracter();
+        }
+    }
+    
+
+     
+  if (askLowerCase){
+        for(var i = 0, n = question; i < question; i++){
+            generated += lowerCase.charAt(Math.floor(Math.random() * n));  
+            document.getElementById("password").innerHTML = generated;     
+                         
+        }
+
+    }
+   
+     else  {
+        
+        upperCase1();
+     }
+
   
-}
+    }
+
+    function copy1(){
+       
+        var copy = document.getElementById("password");
+        copy.select();
+        document.execCommand("copy");
+    }
+        
